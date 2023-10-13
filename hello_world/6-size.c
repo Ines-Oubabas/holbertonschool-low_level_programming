5cbfd2c4403a329180611f4182e59e037d6e2e54
Ines-Oubabas <inesoubabas56@gmail.com>
2023-10-13 05:30:41 -0700

    test 6

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
  * main - Entry point
  *
@@ -8,10 +7,10 @@
 int main(void)
 {
 printf("Size of char: %zu bytes\n", sizeof(char));
-printf("Size of int: %zu bytes\n", sizeof(int));
+printf("Size of an int: %zu bytes\n", sizeof(int));
 printf("Size of float: %zu bytes\n", sizeof((float));
 printf("Size of double: %zu bytes\n", sizeof(double));
-printf("Size of long: %zu bytes\n", sizeof(long));
-printf("Size of long long: %zu bytes\n", sizeof(long long));
+printf("Size of long int: %zu bytes\n", sizeof(long));
+printf("Size of long long int: %zu bytes\n", sizeof(long long));
 return (0);
 }
