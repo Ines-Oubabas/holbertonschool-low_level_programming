7e72c580512578b6eda56c2975bd7aceaafb85d6
Ines-Oubabas <inesoubabas56@gmail.com>
2023-10-16 13:11:30 -0700

    test 6

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int number;
+
+	for (number = 0; number < 10; number++)
+	{
+	putchar(number);
+	}
+	putchar('\n');
+	return (0);
+}
