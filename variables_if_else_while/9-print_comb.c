824cbbac991297e72f456ee8e12bc478c29a9c92
Ines-Oubabas <inesoubabas56@gmail.com>
2023-10-17 02:00:12 -0700

    test 9

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+/**
+ * main - Entry point
+ *
+#include <stdlib.h>
+
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = '0'; i < '9'; i++)
+	{
+	putchar(i);
+	putchar(',');
+	putchar(' ');
+	}
+	putchar('9');
+	return (0);
+}
+
