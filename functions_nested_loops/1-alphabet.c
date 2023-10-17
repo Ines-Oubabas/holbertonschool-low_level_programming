eefe96911d3065499f5ec7c4ddb5b7de32396cb8
Ines-Oubabas <inesoubabas56@gmail.com>
2023-10-17 07:49:31 -0700

    test 1

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_alphabet - use prototype
+ *
+ */
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+		_putchar(c);
+	_putchar('\n');
+}
