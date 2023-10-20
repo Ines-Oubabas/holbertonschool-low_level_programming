44e229566c4ed27c70e69b5247cc1aa2add64f1b
Ines-Oubabas <inesoubabas56@gmail.com>
2023-10-20 07:06:11 -0700

    test 9

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - check the code
+ * @n: the character to check
+ * Return: Fizz, Buzz, or FizzBuzz.
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 1; n <= 100; n++)
+	{
+		if ((n % 3) == 0 && (n % 5) == 0)
+			printf("FizzBuzz");
+		else if ((n % 3) == 0)
+			printf("Fizz");
+		else if ((n % 5) == 0)
+			printf("Buzz");
+		else
+			printf("%d", n);
+		printf(" ");
+	}
+	putchar('\n');
+	return (0);
+}
