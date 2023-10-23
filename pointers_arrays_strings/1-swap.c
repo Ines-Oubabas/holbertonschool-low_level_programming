e7bf82d9c40b69e9378ba5bda55ddbee6eedf410
Ines-Oubabas <inesoubabas56@gmail.com>
2023-10-23 02:10:09 -0700

    test 1

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * swap_int - check the code
+ * @a: character to be checked
+ * @b: character to be checked
+ * Return: the value of two integers
+ */
+void swap_int(int *a, int *b)
+{
+	if (a != 0 && b != 0)
+	{
+		*a = *b;
+		*b = *a;
+	}
+}
