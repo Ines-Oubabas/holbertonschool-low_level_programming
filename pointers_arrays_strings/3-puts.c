c1d8777d0095a3898e0542016f187339d127613b
Ines-Oubabas <inesoubabas56@gmail.com>
2023-10-23 10:52:09 -0700

    test 3

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -3,21 +3,14 @@
 /**
  * _puts - check the code
  * @*str: character to be checked
- * @str: character to be checked
  * Return: print a string, followed by a new line
  */
 void _puts(char *str)
 {
-	int length = 0;
-
-	while (*str != 0)
+	while (*str != '\0')
 	{
-		length++;
+		_putchar(*str);
 		str++;
 	}
-	{
-		length++;
-		_putchar(*str);
-	}
-	_putchar('\n');
+	putchar('\n');
 }
