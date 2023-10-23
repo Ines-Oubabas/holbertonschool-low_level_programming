c85fbd1574b2a82d7c6f3562dcf69f82e4d4cc60
Ines-Oubabas <inesoubabas56@gmail.com>
2023-10-23 01:49:26 -0700

    test 0

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * reset_to_98 - check the code
+ * @n: character to be checked
+ * Return: Always 98
+ */
+void reset_to_98(int *n)
+{
+	if (n != 0)
+	{
+		*n = 98;
+	}
+}
