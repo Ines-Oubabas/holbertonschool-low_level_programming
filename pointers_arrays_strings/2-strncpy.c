10c5df0b14fef629b15746cb0a2513926d0c2c50
Ines-Oubabas <inesoubabas56@gmail.com>
2023-10-25 06:30:10 -0700

    test 2

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * *_strncpy - entry point
+ * @dest: character to check
+ * @src: pointer
+ * @n: var
+ * Return: copie string 
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	char *tmp1 = dest;
+	char *tmp2 = src;
+
+	while (src < tmp2 + n && *src != '\0')
+	{
+		*dest = *src;
+		src++;
+		dest++;
+	}
+	while (src < tmp2 + n)
+	{
+		*dest = '\0';
+		dest++;
+		src++;
+	}
+	dest = tmp1;
+	src = tmp2;
+
+	return (dest);
+}
+
