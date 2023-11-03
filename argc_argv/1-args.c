39844827cde3a46f3bfc9bc008b42d4e9fd0e82d
Ines-Oubabas <inesoubabas56@gmail.com>
2023-11-03 09:15:31 -0700

    test 1

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- * main -  that prints the number of arguments passed into it
- * @argc: variable
- * @argv: variable to check
- * return: always zero
+* main -  that prints the number of arguments passed into it
+* @argc: variable
+* @argv: variable to check
+* Return: always zero
 */
-int main(int argc, __attribute__((unused)) char* argv[])
+int main(int argc, __attribute__((unused)) char *argv[])
 {
 		printf("%d\n", argc - 1);
 	return (0);
