13d803ccc82178e4058f7c17a742524cad99878a
Ines-Oubabas <inesoubabas56@gmail.com>
2023-11-06 06:12:52 -0800

    test 0

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+* create_array - creates an array of chars
+* @size: character to check
+* @c: variable
+* Return: always a
+*/
+char *create_array(unsigned int size, char c)
+{
+	unsigned int i;
+	char *a;
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	a = malloc(size * sizeof(char));
+	if (a == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < size; i++)
+	{
+		a[i] = c;
+	}
+	return (a);
+}
