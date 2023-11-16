6e190e854170cb95fa3b917db9972e9a65783144
Ines-Oubabas <inesoubabas56@gmail.com>
2023-11-16 00:51:14 -0800

    fixe error

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -13,7 +13,7 @@ void print_dog(struct dog *d)
 			printf("Name: %s\n", d->name);
 	else
 		printf("Name: (nil)\n");
-	printf("Age: %s\n", d->name);
+	printf("Age: %f\n", d->age);
 	if (d->owner)
 		printf("Owner: %s\n", d->owner);
 	else
