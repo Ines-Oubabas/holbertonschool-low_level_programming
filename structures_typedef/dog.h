253f6ca02e27c9cbafd402e51acde5aba664afc9
Ines-Oubabas <inesoubabas56@gmail.com>
2023-11-16 00:44:17 -0800

    fixe error

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,5 +1,11 @@
 #ifndef DOG_H
 #define DOG_H
+/**
+* struct dog - Creates a dog
+* @name: The name of the dog
+* @age: The age of the dog
+* @owner: The owner of the dog
+*/
 typedef struct dog
 {
 char *name;
