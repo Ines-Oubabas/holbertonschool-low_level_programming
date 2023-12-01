4bcab9de182089486398cf498ea2ef58edf5e8f5
Ines-Oubabas <inesoubabas56@gmail.com>
2023-12-01 08:07:48 -0800

    test1

--- bit_manipulation/1-print_binary.c
+++ bit_manipulation/1-print_binary.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * print_binary - prints the binary representation of a number.
- * @n: the number to be printed in binary 
+ * @n: the number to be printed in binary
  * Return : nothing
  */
 void print_binary(unsigned long int n)
